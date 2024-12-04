b0fe53038cfa0e6fa1a376e2092a4649181ba9b4
saif-sohel <saifulislamsohel30@gmail.com>
2024-12-04 11:28:18 +0000

    udon: rising for 15

--- lineage_udon.mk
+++ lineage_udon.mk
@@ -11,15 +11,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from udon device
 $(call inherit-product, device/oneplus/udon/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common rising stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1440
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+TARGET_HAS_UDFPS := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
 
 PRODUCT_NAME := lineage_udon
 PRODUCT_DEVICE := udon
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := CPH2487
-HYACINTH_BUILDTYPE := OFFICIAL
 
 PRODUCT_SYSTEM_NAME := CPH2487
 PRODUCT_SYSTEM_DEVICE := OP5961L1
@@ -30,5 +36,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc= "CPH2487-user 13 SKQ1.220905.001 R4T3.1057738-62b8-3b9 release-keys" \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+    RisingChipset="SnapDragon 8+ Gen 1" \
+    RisingMaintainer="Saif Sohel"
 
 BUILD_FINGERPRINT := OnePlus/CPH2487/OP5961L1:13/SKQ1.221119.001/T.R4T3.1057738-62b8-3b9:user/release-keys
