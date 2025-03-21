589b5b0107a817aa3e6b2575ff6ea56aa8f21665
saif-sohel <saifulislamsohel30@gmail.com>
2025-03-21 20:18:05 +0600

    udon: adapt to PixelOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_udon.mk
+    $(LOCAL_DIR)/aosp_udon.mk
 
-COMMON_LUNCH_CHOICES := \
-    lineage_udon-user \
-    lineage_udon-userdebug \
-    lineage_udon-eng
