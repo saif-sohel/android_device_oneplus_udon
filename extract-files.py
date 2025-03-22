33b26b06f46651402b0a9891f9b9574ce0b95ffe
saif-sohel <saifulislamsohel30@gmail.com>
2025-03-22 13:31:42 +0600

    udon: move to py extract scripts

---
+++ extract-files.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/oneplus/sm8450-common',
+]
+
+# Device-specific blob fixups, if any
+blob_fixups: blob_fixups_user_type = {
+}
+
+module = ExtractUtilsModule(
+    'udon',  # device codename
+    'oneplus',  # vendor
+    namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    check_elf=True,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8450-common', module.vendor
+    )
+    utils.run()
