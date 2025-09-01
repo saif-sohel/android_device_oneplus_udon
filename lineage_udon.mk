e0035367c8daeeef58146b6847d7e9efb3219fc5
saif-sohel <saifulislamsohel30@gmail.com>
2025-09-01 10:05:59 +0000

    udon: adapt to latest Rising

--- lineage_udon.mk
+++ lineage_udon.mk
@@ -5,7 +5,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from udon device
@@ -16,10 +16,8 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1440
 WITH_GMS := true
-TARGET_CORE_GMS := true
-TARGET_CORE_GMS_EXTRAS := true
+TARGET_USES_PICO_GAPPS := true
 TARGET_HAS_UDFPS := true
-TARGET_DEFAULT_PIXEL_LAUNCHER := false
 
 PRODUCT_NAME := lineage_udon
 PRODUCT_DEVICE := udon
@@ -33,10 +31,11 @@ PRODUCT_SYSTEM_DEVICE := OP5961L1
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc= "CPH2487-user 13 SKQ1.220905.001 R4T3.1057738-62b8-3b9 release-keys" \
+    BuildFingerprint=OnePlus/CPH2487/OP5961L1:15/AP3A.240617.008/1752583231596:user/release-keys \
+    BuildDesc="CPH2487-user 15 AP3A.240617.008 1752583231596 release-keys"
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
-    RisingChipset="SnapDragon 8+ Gen 1" \
-    RisingMaintainer="Saif Sohel"
-
-BUILD_FINGERPRINT := OnePlus/CPH2487/OP5961L1:13/SKQ1.221119.001/T.R4T3.1057738-62b8-3b9:user/release-keys
+    SystemDevice=$(PRODUCT_SYSTEM_DEVICE) \
+    SystemName=$(PRODUCT_SYSTEM_NAME) \
+    RisingChipset="Snapdragon 8+ Gen 1" \
+    RisingMaintainer="saif-sohel"
